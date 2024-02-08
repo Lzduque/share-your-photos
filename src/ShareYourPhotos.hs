8f74d5d16bfb097e603567dca40f58841764d173
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-08 16:34:54 -0500

    Update ShareYourPhotos.hs

--- src/ShareYourPhotos.hs
+++ src/ShareYourPhotos.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ShareYourPhotos where
+
+import Web.Scotty
 
 shareyourphotos :: IO ()
 shareyourphotos =
-  putStrLn "ShareYourPhotos"
+  scotty 3000 $
+    get "/" $
+      html "<h1>Share Your Photos</h1>"
