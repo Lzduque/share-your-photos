c2cf9053f0e841c4a38cd32245deac0868bc7cb1
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-09 15:15:12 -0500

    Update ShareYourPhotos.hs

--- src/ShareYourPhotos.hs
+++ src/ShareYourPhotos.hs
@@ -2,11 +2,22 @@
 
 module ShareYourPhotos where
 
-import Network.HTTP.Types (status200)
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
 import Web.Scotty
 
 shareyourphotos :: IO ()
-shareyourphotos = scotty 3000 $ do
-  post "/webhook" $ do
-    status status200
-    text "Webhook received!"
+shareyourphotos = do
+  -- Fetch the token from the environment variable
+  token <- lookupEnv "TOKEN"
+  let tokenValue = fromMaybe "" token -- Use a default value if TOKEN is not found
+  scotty 3000 $ do
+    get "/webhook" $ do
+      mode <- param "hub.mode" `rescue` (\_ -> return "")
+      verifyToken <- param "hub.verify_token" `rescue` (\_ -> return "")
+      challenge <- param "hub.challenge" `rescue` (\_ -> return "")
+
+      -- Check if the mode is 'subscribe' and the token matches
+      if mode == "subscribe" && verifyToken == tokenValue
+        then text challenge -- Respond with the challenge if the verification is successful
+        else status 400 -- Send a 400 status code if verification fails
