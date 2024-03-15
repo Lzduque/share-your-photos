1e78584c119e37d73a22b2ca981ad50f056c8c44
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-03-15 16:42:38 -0400

    wip: send the entire div row to the server

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,38 +1,52 @@
-// Function to handle image extraction and sending URLs to the background script
+// Function to handle rows extraction and sending them to the background script
 function handleImageExtraction() {
-	const images = document.querySelectorAll('img') // Adjust selector as needed
-	images.forEach((img) => {
-		// Send each image URL to the background script
-		chrome.runtime.sendMessage({imageUrl: img.src})
-	})
+	const main = document.querySelector('div[id="main"]')
+	const objects = [...main.querySelectorAll('div[role="row"]')]
+
+	if (objects) {
+		console.log('objects: ', objects)
+		objects.forEach((obj) => {
+			// Send the entire div row to the haskell server
+			console.log('object: ', obj)
+			chrome.runtime.sendMessage({row: obj})
+		})
+	}
 }
 
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.action === 'startObserving') {
-		// Extract and send existing images immediately
+		// Extract and send existing rows immediately
 		console.log('3. Received startObserving')
 		handleImageExtraction()
 
-		// Create a MutationObserver to watch for added images
+		// Create a MutationObserver to watch for added <div class role="row">
 		const observer = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
-				mutation.addedNodes.forEach((node) => {
-					// Check if the added node is an image or contains images
-					if (node.nodeName === 'IMG') {
-					// console.log('3. Got mutations')
-						chrome.runtime.sendMessage({imageUrl: node.src})
-					} else if (node.querySelectorAll) {
-						node.querySelectorAll('img').forEach((img) => {
-							chrome.runtime.sendMessage({imageUrl: img.src})
-						})
-					}
-				})
+				if (mutation.addedNodes.length) {
+					mutation.addedNodes.forEach((node) => {
+						if (
+							node.nodeName === 'DIV' &&
+							node.getAttribute('role') === 'row'
+						) {
+							console.log('Mutation node: ', node)
+							chrome.runtime.sendMessage({
+								row: node,
+							})
+							// }
+						}
+					})
+				}
 			})
 		})
 
 		// Configuration of the observer:
-		const config = {childList: true, subtree: true}
+		const config = {
+			childList: true, // Set to true to observe for the addition/removal of child nodes
+			characterData: true, // Observe data changes in text nodes
+			attributes: true, // Set to true to observe attribute changes
+			subtree: true, // Set to true to observe descendants of the target node
+		}
 
 		// Start observing the target node for configured mutations
 		observer.observe(document.body, config)
