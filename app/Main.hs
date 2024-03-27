42687715118e39b41815c73a161632a2e28089e6
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-03-27 16:25:32 -0400

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -5,65 +5,64 @@
 module Main where
 
 import GHC.Generics (Generic)
-import Data.Aeson (FromJSON, object, (.=), encode, decode)
-import Data.ByteString.Lazy (ByteString)
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text.Lazy.Encoding as LTE
 import qualified Data.Text as T
-import Text.HTML.TagSoup
+import qualified Text.HTML.TagSoup as TS
 import qualified Data.Set as Set
-import Data.IORef (newIORef, IORef, readIORef, modifyIORef)
-import Web.Scotty
-import Control.Monad.IO.Class (liftIO)
-import Network.HTTP.Types.Header 
+import qualified Data.IORef as IORef
+import qualified Web.Scotty as Scotty
+import qualified Control.Monad.IO.Class as MIO
 
 data ImageRequest = ImageRequest
     { row :: T.Text
-    } deriving (Show, Generic, FromJSON)
+    } deriving (Show, Generic, A.FromJSON)
 
 main :: IO ()
 main = do
-    imageSetRef <- newIORef Set.empty  -- Initialize the IORef to an empty Set
+    imageSetRef <- IORef.newIORef Set.empty  -- Initialize the IORef to an empty Set
     putStrLn "Starting server on port 3001..."
-    scotty 3001 $ app imageSetRef
+    Scotty.scotty 3001 $ app imageSetRef
 
-app :: IORef (Set.Set T.Text) -> ScottyM ()
+app :: IORef.IORef (Set.Set T.Text) -> Scotty.ScottyM ()
 app imageSetRef = do
     -- Add OPTIONS route for preflight requests
-    options (regex ".*") $ do
-        setHeader "Access-Control-Allow-Origin" "*"
-        setHeader "Access-Control-Allow-Methods" "POST, OPTIONS"
-        setHeader "Access-Control-Allow-Headers" "Content-Type"
+    Scotty.options (Scotty.regex ".*") $ do
+        Scotty.setHeader "Access-Control-Allow-Origin" "*"
+        Scotty.setHeader "Access-Control-Allow-Methods" "POST, OPTIONS"
+        Scotty.setHeader "Access-Control-Allow-Headers" "Content-Type"
 
-    post "/send-image" $ do
-        setHeader "Access-Control-Allow-Origin" "*"
-        b <- body
-        liftIO $ putStrLn $ "Received body: " ++ show b  -- Debug print
-        response <- liftIO $ echoJson imageSetRef b
-        setHeader "Content-Type" "application/json"
-        raw response
+    Scotty.post "/send-image" $ do
+        Scotty.setHeader "Access-Control-Allow-Origin" "*"
+        b <- Scotty.body
+        MIO.liftIO $ putStrLn $ "1. Received body: " ++ show b  -- Debug print
+        response <- MIO.liftIO $ extractImages imageSetRef b
+        Scotty.setHeader "Content-Type" "application/json"
+        Scotty.raw response
 
-echoJson :: IORef (Set.Set T.Text) -> ByteString -> IO ByteString
-echoJson imageSetRef bs = do
-    let req = decode bs :: Maybe ImageRequest  -- Use bs directly
+extractImages :: IORef.IORef (Set.Set T.Text) -> BS.ByteString -> IO BS.ByteString
+extractImages imageSetRef bs = do
+    let req = A.decode bs :: Maybe ImageRequest  -- Use bs directly
     case req of
         Just r -> do
-            liftIO $ putStrLn $ "Decoded request: " ++ show r  -- Debug print
-            let tags = parseTags $ row r
-            liftIO $ putStrLn $ "Parsed tags: " ++ show tags  -- Debug print
-            let imgSrcs = Set.fromList [srcValue | TagOpen "img" attrs <- tags, ("src", srcValue) <- attrs, "blob:" `T.isPrefixOf` srcValue]
-            liftIO $ putStrLn $ "Image sources: " ++ show imgSrcs  -- Debug print
-            alreadyStored <- readIORef imageSetRef
-            liftIO $ putStrLn $ "Already stored: " ++ show alreadyStored  -- Debug print
+            -- MIO.liftIO $ putStrLn $ "a 2. Decoded request: " ++ show r  -- Debug print
+            let tags = TS.parseTags $ row r
+            MIO.liftIO $ putStrLn $ "a 3. Parsed tags: " ++ show tags  -- Debug print
+            let imgSrcs = Set.fromList [srcValue | TS.TagOpen "img" attrs <- tags, ("src", srcValue) <- attrs, "blob:" `T.isPrefixOf` srcValue]
+            -- MIO.liftIO $ putStrLn $ "a 4. Image sources: " ++ show imgSrcs  -- Debug print
+            alreadyStored <- IORef.readIORef imageSetRef
+            -- MIO.liftIO $ putStrLn $ "a 5. Already stored: " ++ show alreadyStored  -- Debug print
             let newSrcs = Set.difference imgSrcs alreadyStored
-            modifyIORef imageSetRef (`Set.union` newSrcs)
-            liftIO $ putStrLn $ "New sources: " ++ show newSrcs  -- Debug print
+            IORef.modifyIORef imageSetRef (`Set.union` newSrcs)
+            MIO.liftIO $ putStrLn $ "a 6. New sources: " ++ show newSrcs  -- Debug print
             if Set.null newSrcs
                 then do
-                    liftIO $ putStrLn "No new images"  -- Debug print
-                    return $ encode $ object []
+                    MIO.liftIO $ putStrLn "a 7 a. No new images"  -- Debug print
+                    return $ A.encode $ A.object []
                 else do
-                    liftIO $ putStrLn $ "Adding new images: " ++ show (Set.toList newSrcs)  -- Debug print
-                    return $ encode $ object ["imageUrls" .= Set.toList newSrcs]
+                    MIO.liftIO $ putStrLn $ "a 7 b. Adding new images: " ++ show (Set.toList newSrcs)  -- Debug print
+                    return $ A.encode $ A.object ["imageUrls" A..= Set.toList newSrcs]
         Nothing -> do
-            liftIO $ putStrLn "Failed to decode JSON"  -- Debug print
-            return $ encode $ object ["error" .= ("Failed to decode JSON" :: T.Text)]
+            MIO.liftIO $ putStrLn "b 1. Failed to decode JSON"  -- Debug print
+            return $ A.encode $ A.object ["error" A..= ("Failed to decode JSON" :: T.Text)]
