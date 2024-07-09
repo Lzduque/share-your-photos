ba52e54be1ac33c34e21cd7a43c965895c124402
SlimTim10 <slimtim10@gmail.com>
2024-07-09 19:31:37 -0400

    Progress

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,65 +1,79 @@
 // Keep track of the animation tab ID
 let animationTabId = null
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  // console.log('Message received: ', message)
+// images :: [{id :: String, blobUrl :: String}]
+let images = []
+
+const startMonitoringWhatsApp = () => {
+  // Open the animation page in a new tab
+  chrome.tabs.create(
+    {url: chrome.runtime.getURL('animation.html')},
+    (tab) => {
+      animationTabId = tab.id
+    }
+  )
+  // Find the WhatsApp Web tab
+  chrome.tabs.query({url: '*://web.whatsapp.com/*'}, (tabs) => {
+    if (tabs.length >= 1) {
+      const tab = tabs[0]
+      // Inject the content script into the WhatsApp Web tab
+      console.log('2. inject script')
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['scripts/content.js'],
+      }, () => {
+        // Once the content script is injected, send a message to start observing
+        console.log('2. Send startObserving')
+        chrome.tabs.sendMessage(tab.id, {
+          action: 'startObserving',
+        })
+      })
+    }
+  })
+}
+
+chrome.runtime.onMessage.addListener(async (message, _sender, _sendResponse) => {
   if (message.action === 'startMonitoringWhatsApp') {
-    // Open the animation page in a new tab
     console.log('1. startMonitoringWhatsApp')
     console.log('1. Message: ', message)
-    chrome.tabs.create(
-      {url: chrome.runtime.getURL('animation.html')},
-      (tab) => {
-        animationTabId = tab.id
-      }
-    )
-    // Find the WhatsApp Web tab
-    chrome.tabs.query({url: '*://web.whatsapp.com/*'}, (tabs) => {
-      tabs.forEach((tab) => {
-        // Inject the content script into the WhatsApp Web tab
-        console.log('2. inject script')
-        chrome.scripting.executeScript(
-          {
-            target: {tabId: tab.id},
-            files: ['scripts/content.js'],
-          },
-          () => {
-            // Once the content script is injected, send a message to start observing
-            console.log('2. Send startObserving')
-            chrome.tabs.sendMessage(tab.id, {
-              action: 'startObserving',
-            })
-          }
-        )
-      })
-    })
-  } else if (message.content) {
-    const body = JSON.stringify({content: message.content})
-    console.log('4. Message body sent: ', body)
+    startMonitoringWhatsApp()
+  } else if (message.images) {
+    // Receiving HTML content from WhatsApp Web tab
+    
+    // TODO: parse out all images
+    console.log('message.content:', message.content)
 
-    try {
-      console.log('Fetching...')
-      const response = await fetch('http://localhost:3001/send-image', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: body,
-      })
-      const data = await response.json()
-      console.log('5. Data: ', data)
-      // When receiving an image URL, forward it to the animation tab
-      if (data.imageUrls && animationTabId !== null) {
-        const newImages = data.imageUrls
-        console.log('7. newImages: ', newImages)
-        // console.log('7. animationTabId: ', animationTabId)
-        newImages.map((e) =>
-          chrome.tabs.sendMessage(animationTabId, {
-            image: e,
-          })
-        )
+    // TODO: add new images to current list of images
+
+    // OLD
+    if (false) {
+      const body = JSON.stringify({content: message.content})
+      console.log('4. Message body sent: ', body)
+
+      try {
+        console.log('Fetching...')
+        const response = await fetch('http://localhost:3001/send-image', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: body,
+        })
+        const data = await response.json()
+        console.log('5. Data: ', data)
+        // When receiving an image URL, forward it to the animation tab
+        if (data.imageUrls && animationTabId !== null) {
+          const newImages = data.imageUrls
+          console.log('7. newImages: ', newImages)
+          // console.log('7. animationTabId: ', animationTabId)
+          newImages.map((e) =>
+            chrome.tabs.sendMessage(animationTabId, {
+              image: e,
+            })
+          )
+        }
+      } catch (err) {
+        console.log('Error: ', err)
+        console.error(err)
       }
-    } catch (err) {
-      console.log('Error: ', err)
-      console.error(err)
     }
   }
 })
--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,47 +1,77 @@
-const handleImageExtraction = () => {
-  const main = document.querySelector('div[id="main"]')
+// Use IIFE to avoid global scope issues
+(function() {
+  let imageDB = {}
 
-  if (main) {
-    // console.log('objects: ', objects)
-    chrome.runtime.sendMessage({
-      content: main.innerHTML,
-    })
-  }
-}
+  // TODO: remove this
+  const handleImageExtraction = () => {
+    const imageNodes = document.querySelectorAll('div[id="main"] div[role="row"] img[src^="blob:https://web.whatsapp.com/"]')
+    const images = Array.from(imageNodes).map(img => img.src)
 
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'startObserving') {
-    handleImageExtraction()
-
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((m) => {
-        // console.log('Mutation className: ', m.addedNodes[0].className)
-        if (
-          m.type === 'childList' &&
-            m.addedNodes.length > 0 &&
-            m.addedNodes[0] &&
-            m.addedNodes[0].className === '' &&
-            m.addedNodes[0].tagName === 'DIV'
-        ) {
-          setTimeout(() => {
-            chrome.runtime.sendMessage({
-              content: document.querySelector('div[id="main"]').innerHTML,
-            })
-          }, 240)
-        }
+    if (images.length > 0) {
+      chrome.runtime.sendMessage({
+        images
       })
-    })
-
-    // Configuration of the observer:
-    const config = {
-      childList: true, // Observe for the addition/removal of child nodes
-      characterData: true, // Observe data changes in text nodes
-      attributes: true, // Observe attribute changes
-      subtree: true, // Observe descendants of the target node
     }
-
-    // Start observing the target node for configured mutations
-    observer.observe(document.body, config)
   }
-})
+
+  // Is this the kind of mutation that has new images in the chat?
+  const goodMutation = m => {
+    return (
+      m.type === 'childList' &&
+        m.addedNodes.length > 0 &&
+        m.addedNodes[0] &&
+        m.addedNodes[0].className === '' &&
+        m.addedNodes[0].tagName === 'DIV'
+    )
+  }
+
+  // Get new image within the node and add id to the DB
+  const addNewImage = (node) => {
+    const id = node.firstElementChild?.getAttribute('data-id')
+    if (id) {
+      const images = node.querySelectorAll('img[src^="blob:https://web.whatsapp.com/"]')
+      if (images.length > 0) {
+        const lastImage = {
+          id,
+          src: images[images.length - 1].src
+        }
+        imageDB[id] = lastImage
+        console.log(imageDB)
+      }
+    }
+  }
+
+  // Listen for messages from the popup
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'startObserving') {
+      // TODO: Don't forget to send images on first load
+
+      // OLD:
+      // handleImageExtraction()
+
+      const observer = new MutationObserver(mutations => {
+        mutations
+          .filter(goodMutation)
+          .forEach(m => {
+            Array.from(m.addedNodes).forEach(node => {
+              // Wait for the DOM to update with blobs
+              setTimeout(() => {
+                addNewImage(node)
+              }, 1000)
+            })
+          })
+      })
+
+      // Configuration of the observer:
+      const config = {
+        childList: true, // Observe for the addition/removal of child nodes
+        characterData: true, // Observe data changes in text nodes
+        attributes: true, // Observe attribute changes
+        subtree: true, // Observe descendants of the target node
+      }
+
+      // Start observing the target node for configured mutations
+      observer.observe(document.body, config)
+    }
+  })
+})();
